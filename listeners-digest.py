06bae7086ba9e502f0c79d3ac8c6a1cfe17991e9
Will <will@wiri.co.uk>
2023-04-06 07:24:07 +0100

    wireframe class

---
+++ listeners-digest.py
@@ -0,0 +1,20 @@
+class URLToAudio:
+    """This class """
+    def __init__(self):
+        pass
+
+    def __parse_request_string(self):
+        """processes passed string and ensures format is suitable to submit to Wikipedia API"""
+        pass
+    
+    def __getWikipediaContent(self):
+        """"""
+        pass
+
+    def __request_speech_synthesis(self):
+        """"""
+        pass
+
+    def play_audio(self):
+        """"""
+        pass
